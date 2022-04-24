e8377c112be5bfd852dd71400c5dc3afaa67b574
Jesus-quintanilla <99986821+Jisus-quintanilla@users.noreply.github.com>
2022-04-23 19:59:44 -0600

    Create Programa_Rifa.cs

---
+++ Programa_Rifa.cs
@@ -0,0 +1,6 @@
+using Rifa1.Clases;
+
+Rifa2 arr = new Rifa2();
+
+string[] Sor = { "Juan", "Pablo", "Fernando", "Jesus", "Gilberto", "Samuel", "Andrew", "Sergio", "Cristian", "Bryan", "Alan", "Jaime", "Ronaldo" };
+arr.Sorteo(Sor, 2);
